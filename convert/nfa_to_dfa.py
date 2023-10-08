6fd0f17d9da58424bed5583570873cc6139df08b
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2023-10-08 09:14:38 +0800

    feat: used dictionary instead of list

--- convert/nfa_to_dfa.py
+++ convert/nfa_to_dfa.py
@@ -7,12 +7,18 @@ class NfaToDfaConverter:
         self.nfa_transition_table = None
         self.inputs = None
         self.states = None
+        self.start_state = None
+        self.final_states = None
         self.method = None
+        self.powerset = None
 
-    def convert(self, inputs, states, nfa_transition_table, method):
+    def convert(self, inputs, states, start_state, final_states, nfa_transition_table, method):
         self.nfa_transition_table = nfa_transition_table
         self.inputs = inputs
         self.states = states
+        self.start_state = start_state
+        self.final_states = final_states
+        self.dfa_transition_table = None
 
         if method == "subset_construction":
             self._convert_using_subset_construction_method()
@@ -20,33 +26,54 @@ class NfaToDfaConverter:
             self._convert_using_lazy_method()
 
     def _convert_using_subset_construction_method(self):
-        powerset = self._create_powerset(self.states)
-        powerset.append(set())
+        self.powerset = self._create_powerset(self.states)
+        self.powerset.append(set())
 
-        self.dfa_transition_table = [[None] * len(self.inputs) for i in range(pow(2, len(self.states)))]
+        self.dfa_transition_table = {frozenset(state): {} for state in self.powerset}
 
-        for i in range(len(powerset)):
-            sstates = powerset[i]
-            for j in range(len(self.inputs)):
-                curr_input = self.inputs[j]
+        for set_of_states in self.powerset:
+            for i in self.inputs:
                 output_states = []
-                for state in sstates:
-                    output_states += self.nfa_transition_table[self.states.index(state)][self.inputs.index(curr_input)]
-                self.dfa_transition_table[i][j] = set(sorted(set(output_states)))
-
-        for i in range(len(self.dfa_transition_table)):
-            self.dfa_transition_table[i].insert(0, powerset[i])
+                for state in set_of_states:
+                    output_states += self.nfa_transition_table[state][i]
+                self.dfa_transition_table[frozenset(set_of_states)][i] = set(sorted(output_states))
 
         self._print_dfa_transition_table()
 
     def _convert_using_lazy_method(self):
-        pass
+        self.dfa_transition_table = [[None] * len(self.inputs) for i in range(len(self.states))]
+        self.dfa_transition_table[0] = list(map(set, self.nfa_transition_table[0]))
+        new_states = [self.start_state]
+        curr_row = 0
+
+        while len(new_states) > 0:
+            curr_state = new_states.pop(0)
+            for i in range(len(self.inputs)):
+                curr_input = self.inputs[i]
+                output_states = []
+                for state in curr_state:
+                    output_states += self.nfa_transition_table[self.states.index(state)][self.inputs.index(curr_input)]
+                output_states = set(sorted(set(output_states)))
+                if output_states not in new_states:
+                    self.dfa_transition_table.append(output_states)
+                    new_states.append(output_states)
+                self.dfa_transition_table[curr_row][i] = output_states
+            curr_row += 1
+        
+        self._print_dfa_transition_table()
 
     def _create_powerset(self, q):
         powerset = chain.from_iterable(combinations(q, r) for r in range (1, len(q) + 1))
         return list(map(set, powerset))
         
     def _print_dfa_transition_table(self):
+        table_body = []
+        for state in self.powerset:
+            row = [state]
+            for i in self.inputs:
+                row += [self.dfa_transition_table[frozenset(state)][i]]
+            table_body.append(row)
+
         print("\nDFA Transition Table:")
-        print(tabulate(self.dfa_transition_table, headers = self.inputs, tablefmt='fancy_grid'))
+        print(tabulate(table_body, headers = self.inputs, tablefmt='fancy_grid'))
 
--- main.py
+++ main.py
@@ -4,21 +4,27 @@ from convert.nfa_to_dfa import NfaToDfaConverter
 def main():
     inputs = input("Enter input symbols (separated by white spaces): ").split()
     states = input("Enter states (separated by white spaces): ").split()
+    start_state = input("Enter start state: ")
+    final_states = input("Enter accepting state/s (separated by white spaces): ").split()
+
     nfa_transition_table = get_nfa_transition_table(inputs, states)
 
+    print(nfa_transition_table)
+
+    pass
     converter = NfaToDfaConverter()
-    converter.convert(inputs, states, nfa_transition_table, "subset_construction")
+    converter.convert(inputs, states, start_state, final_states, nfa_transition_table, "subset_construction")
 
 
 def get_nfa_transition_table(inputs, states):
-    nfa_transition_table = [[None] * len(inputs) for i in range(len(states))]
+    nfa_transition_table = {state: {} for state in states}
  
-    for i in range(len(inputs)):
-        print("Enter the set of states that can be reached from state q following only arcs labeled", inputs[i])
-        for j in range(len(states)):
-            print(states[j], "-> ", end = "")
+    for i in inputs:
+        print("Enter the set of states that can be reached from state q following only arcs labeled", input)
+        for s in states:
+            print(s, "-> ", end = "")
             output_states = input().split()
-            nfa_transition_table[j][i] = output_states
+            nfa_transition_table[s][i] = output_states
 
     return nfa_transition_table
 
