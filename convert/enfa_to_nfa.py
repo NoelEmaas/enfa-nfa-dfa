6e092e68d617fb2bd5901835de0f5b4d08324fb0
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2023-10-08 15:31:47 +0800

    feat: added enfa to nfa conversion

--- convert/enfa_to_nfa.py
+++ convert/enfa_to_nfa.py
@@ -1,3 +1,85 @@
+from tabulate import tabulate
+
 class ENfaToNfaConverter:
     def __init__(self):
-        pass
+        self.nfa_transition_table = None
+        self.enfa_transition_table = None
+        self.inputs = None
+        self.states = None
+        self.start_state = None
+        self.final_states = None
+        self.method = None
+
+    def convert(self, inputs, states, start_state, final_states, enfa_transition_table):
+        self.enfa_transition_table = enfa_transition_table
+        self.inputs = inputs
+        self.states = states
+        self.start_state = start_state
+        self.final_states = final_states
+        self.nfa_transition_table = None
+
+        for state in states:
+            self.enfa_transition_table[state]['CL(q)'] = self._get_closure_states(state)
+            for i in inputs:
+                if i != 'ε':
+                    self.enfa_transition_table[state]['S' + str(i)] = self._getS(i, state)
+
+        for state in states:
+            for i in inputs:
+                if i != 'ε':
+                    si_closure = []
+                    si_states = self.enfa_transition_table[state]['S' + str(i)]
+                    for si_state in si_states:
+                        si_closure += self._get_closure_states(si_state)
+                    self.enfa_transition_table[state]['δN(q,' + str(i) + ')'] = set(si_closure)
+
+        print(self.enfa_transition_table)
+        self._print_nfa_transition_table(self.states)
+
+    def _get_closure_states(self, state):
+        closure_states = set()
+        states_stack = [state]
+
+        while (len(states_stack)):
+            curr_state = states_stack.pop(0)
+            if curr_state not in closure_states:
+                closure_states.add(curr_state)
+            for i in self.enfa_transition_table[curr_state]['ε']:
+                if i not in closure_states:
+                    states_stack.append(i)
+
+        return closure_states
+
+    def _getS(self, input, state):
+        closure_states = self.enfa_transition_table[state]['CL(q)']
+        s = []
+        for state in closure_states:
+            s += self.enfa_transition_table[state][input]
+        return set(s)
+
+    def _print_nfa_transition_table(self, states):
+        headers = ['']
+        for i in self.inputs:
+            headers += [i]
+        headers.append('CL(q)')
+        for i in self.inputs:
+            headers.append('S' + str(i))
+        for i in self.inputs:
+            headers.append('δN(q,' + str(i) + ')')
+        
+        table_body = []
+        for state in states:
+            row = [state]
+            for i in self.inputs:
+                row += [self.enfa_transition_table[state][i]]
+            row += [self.enfa_transition_table[state]['CL(q)']]
+            for i in self.inputs:
+                if i != 'ε':
+                    row += [self.enfa_transition_table[state]['S' + str(i)]]
+            for i in self.inputs:
+                if i != 'ε':
+                    row += [self.enfa_transition_table[state]['δN(q,' + str(i) + ')']]
+            table_body.append(row)
+
+        print("\nENFA to NFA Transition Table:")
+        print(tabulate(table_body, headers = headers, tablefmt='fancy_grid'))
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from convert.nfa_to_dfa import NfaToDfaConverter
-
+from convert.enfa_to_nfa import ENfaToNfaConverter
 
 def main():
     inputs = input("Enter input symbols (separated by white spaces): ").split()
@@ -7,23 +7,22 @@ def main():
     start_state = input("Enter start state: ")
     final_states = input("Enter accepting state/s (separated by white spaces): ").split()
 
-    nfa_transition_table = get_nfa_transition_table(inputs, states)
+    nfa_transition_table = get_transition_table(inputs, states)
 
-    converter = NfaToDfaConverter()
-    converter.convert(inputs, states, start_state, final_states, nfa_transition_table, "lazy")
+    converter = ENfaToNfaConverter()
+    converter.convert(inputs, states, start_state, final_states, nfa_transition_table)
 
-
-def get_nfa_transition_table(inputs, states):
-    nfa_transition_table = {state: {} for state in states}
+def get_transition_table(inputs, states):
+    transition_table = {state: {} for state in states}
  
     for i in inputs:
-        print("Enter the set of states that can be reached from state q following only arcs labeled", input)
+        print("Enter the set of states that can be reached from state q following only arcs labeled", i)
         for s in states:
             print(s, "-> ", end = "")
             output_states = input().split()
-            nfa_transition_table[s][i] = output_states
+            transition_table[s][i] = output_states
 
-    return nfa_transition_table
+    return transition_table
 
 
 if __name__ == "__main__":
